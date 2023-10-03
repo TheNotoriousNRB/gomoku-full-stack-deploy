ec5d57403cd277a5b95f734d653d0ddb5f404d05
Niraj Rana Bhat <nranabha@myune.edu.au>
2023-10-04 02:07:08 +1100

    Updated 2

--- gomoku-service/src/utils/DB.Connect.ts
+++ gomoku-service/src/utils/DB.Connect.ts
@@ -1,7 +1,7 @@
 import mongoose, {mongo} from 'mongoose';
 
 const ConnectDB =async () => {
-    const dbUri = "mongodb+srv://nirajrbrb:96tKnrougSr08FA6@cluster0.odhcvpl.mongodb.net/gomoku?retryWrites=true&w=majority";
+    const dbUri = "mongodb+srv://nirajrbrb:U48r5IVEmpIiF6Cj@cluster0.odhcvpl.mongodb.net/gomoku?retryWrites=true&w=majority";
     mongoose.set('strictQuery', false);
     console.log("Connecting to Database")
     try{
